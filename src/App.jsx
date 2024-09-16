53281cb6a231775fa985b9d8167a2b5cc8567ccb
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-09-16 09:46:06 -0400

    1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Content from './components/Content';
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Header />
+      <Content color="blue" text="This is my first React Application!" />
+      <Content color="red" text="Wish me luck..." />
+      <Content color="green" text="I think I've got it!" />
+      <Footer />
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Content.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Content() {
+    return (<p>This is my first React Application, wish me luck!</p>);
+  }
+
+export default Content
